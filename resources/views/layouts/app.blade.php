0e26c99b6ba40d771769bc9a3c82efb702d57bf9
meritta20 <khadim.meriem@gmail.com>
2020-07-17 15:29:45 +0100

    fach bdlt navbar o zedt l carousel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
 
 	<!-- Fav and Touch Icons -->
 
-	<link rel="shortcut icon"  href="{{ asset('/') }}images/ico/logo-zh-tours.png">
+	<link rel="shortcut icon" href="{{ asset('/') }}images/ico/logo-zh-tours.png">
 
 	<!-- CSS Plugins -->
 	<link rel="stylesheet" type="text/css" href="{{ asset('/') }}bootstrap/css/bootstrap.min.css" media="screen">
@@ -27,13 +27,13 @@
 	<link href="{{ asset('/') }}css/style.css" rel="stylesheet">
 
 	<!-- Add your style -->
-    <link href="{{ asset('/') }}css/your-style.css" rel="stylesheet">
+	<link href="{{ asset('/') }}css/your-style.css" rel="stylesheet">
 
-    <!-- AOS libs -->
-    <link href="{{ asset('/') }}dist/aos.css" rel="stylesheet">
+	<!-- AOS libs -->
+	<link href="{{ asset('/') }}dist/aos.css" rel="stylesheet">
 
-    <!-- FontAwesome Style File -->
-    <link rel="stylesheet" href="{{ asset('/') }}css/all.min.css">
+	<!-- FontAwesome Style File -->
+	<link rel="stylesheet" href="{{ asset('/') }}css/all.min.css">
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
@@ -60,43 +60,70 @@
 
 				<div class="container">
 
-					<div class="logo-wrapper">
+					<div class="logo-wrapper ">
 						<div class="logo">
 							<a href="#"><img src="{{ asset('/') }}images/ico/logo-zh-tours-mx.png" alt="Logo" /></a>
 						</div>
 					</div>
 
-					<div id="navbar" class="navbar-nav-wrapper">
+					<div id="navbar" class="navbar-nav-wrapper pull-right "  >
 
 						<ul class="nav navbar-nav" id="responsive-menu">
-
+  
 							<li>
+<<<<<<< HEAD
 								<a href="{{ asset('/') }}" >Accueil</a>
+=======
+								<a href="{{ route('login') }}">Accueil</a>
+>>>>>>> fach bdlt navbar o zedt l carousel
 							</li>
 
 							<li>
 								<a href="#">Immobilier</a>
 								<ul>
-									<li><a href="#">Immobilier Vente</a></li>
-									<li><a href="#">Immobilier Location</a></li>
-								</ul>
+									<li><a href="#">Immobilier Vente</a>
+									</li>
+									<li><a href="#">Immobilier Location</a>
+
+										<ul>
+											<li><a href="#">Langue durée</a></li>
+											<li><a href="#">Courte durée</a></li>
+											<li><a href="#">Saisonniéres</a></li>
+
+										</ul>
+									</li>
 							</li>
+						</ul>
+						</li>
 
-							<li>
-								<a href="{{ route('car.index',['categorie' => 1]) }}">Voitures de luxe</a>
+						<li>
+							<a href="{{ route('car.index',['categorie' => 1]) }}">Voitures de luxe</a>
 
-							</li>
+						</li>
+						 
+						<li>
+							<a href="{{ route('car.index',['categorie' => 2]) }}">Transport touristique</a>
 
-							<li>
-								<a href="{{ route('car.index',['categorie' => 2]) }}">Transport touristique</a>
+						</li>
+<li>
+<a href="#" class="earth"  > <i class="fa fa-globe " id="earth" aria-hidden="true" style="width: 37;
+    height: 37;  
+"></i> Fr
+<ul>
+											<li><a href="#">Arabe</a></li>
+											<li class="active"><a href="#" class="active"> Français</a></li>
+											<li><a href="#">Anglais</a></li>
 
-							</li>
-
+										</ul>
+</a>
+ 
+</li> 
 
 
 						</ul>
 
-					</div><!--/.nav-collapse -->
+					</div>
+					<!--/.nav-collapse -->
 
 				</div>
 
@@ -111,64 +138,59 @@
 
 		<!-- start Main Wrapper -->
 
-		<div class="main-wrapper scrollspy-container">
-            <!-- Carousel
-                ================================================== -->
-            <div id="myCarousel" class="carousel slide hero" data-ride="carousel">
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                    <img class="first-slide sl-ovelay" src="{{ asset('/') }}storage/uploads-mx/BG-LAMBO.jpg" alt="First slide">
-
-                    <div class="carousel-caption ">
-                        <h1>WELCOME</h1>
-                        <p>BEST PLACE FOR YOU</p>
-                    </div>
-                </div>
-                <div class="item">
-                    <img class="second-slide sl-ovelay" src="{{ asset('/') }}storage/uploads-mx/BG-LAMBO.jpg" alt="Second slide">
-
-                    <div class="carousel-caption">
-                        <h1>WELCOME 2</h1>
-                        <p>BEST CAR FOR YOU</p>
-                    </div>
-                </div>
-                <div class="item">
-                    <img class="third-slide sl-ovelay" src="{{ asset('/') }}storage/uploads-mx/BG-LAMBO.jpg" alt="Third slide">
-
-                    <div class="carousel-caption">
-                        <h1>WELCOME 3</h1>
-                        <p>BEST HOUSE FOR YOU</p>
-                    </div>
-                </div>
-                </div>
-                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-                </a>
-            </div>
-            <!-- /.carousel -->
 
 
 
-                <!-- start our content -->
-                    <main class="py-4">
-                        @yield('content')
-                    </main>
-		</div>
-        <!-- end our content -->
+
+		<!-- start our content -->
+		<main class="py-4">
+			@yield('content')
+		</main>
+
+		<!-- end our content -->
 
 		<!-- end Main Wrapper -->
+		<div class="post-hero pt-80 pb-80 clearfix">
 
+			<div class="container">
+
+				<div class="row">
+
+					<div class="col-xs-12 col-sm-4 mb-20-xs">
+						<div class="horizontal-featured-icon-sm clearfix">
+							<div class="icon"><i class="ri ri-location"></i></div>
+							<div class="content">
+								<h6>Looking for a tour program?</h6>
+								<span>Inhabiting discretion the her dispatched decisively boisterous joy.</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-4 mb-20-xs">
+						<div class="horizontal-featured-icon-sm clearfix">
+							<div class="icon"> <i class="ri ri-user"></i></div>
+							<div class="content">
+								<h6>Need someone to guide tour?</h6>
+								<span>Great asked oh under together prospect kindness securing six.</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-4 mb-20-xs">
+						<div class="horizontal-featured-icon-sm clearfix">
+							<div class="icon"> <i class="ri ri-equal-circle"></i></div>
+							<div class="content">
+								<h6>Want to earn money as guide?</h6>
+								<span>Sometimes studied evident. Conduct replied removal her cordially. </span>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
 		<!-- start Footer Wrapper -->
 
 		<div class="footer-wrapper scrollspy-footer">
@@ -211,36 +233,55 @@
 	<!-- end Container Wrapper -->
 
 
-<!-- start Back To Top -->
+	<!-- start Back To Top -->
 
-<div id="back-to-top">
-   <a href="#"><i class="ion-ios-arrow-up"></i></a>
-</div>
+	<div id="back-to-top">
+		<a href="#"><i class="ion-ios-arrow-up"></i></a>
+	</div>
 
-<!-- end Back To Top -->
-
-
-<!-- AOS libs -->
-
-<script src="{{ asset('/') }}dist/aos.js"></script>
-
-<script>
-    AOS.init();
-  </script>
+	<!-- end Back To Top -->
 
 
+	<!-- AOS libs -->
+
+	<script src="{{ asset('/') }}dist/aos.js"></script>
+
+	<script>
+		AOS.init();
+	</script>
+
+
+<<<<<<< HEAD
 <!-- Core JS -->
 <script type="text/javascript" src="{{ asset('/') }}js/jquery.min.js"></script>
 <script type="text/javascript" src="{{ asset('/') }}bootstrap/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="{{ asset('/') }}js/core-plugins.js"></script>
 <script type="text/javascript" src="{{ asset('/') }}js/customs.js"></script>
 <script type="text/javascript" src="{{ asset('/') }}js/mx.js"></script>
+=======
+	<!-- Core JS -->
+	<script type="text/javascript" src="{{ asset('/') }}js/jquery.min.js"></script>
+	<script type="text/javascript" src="{{ asset('/') }}bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="{{ asset('/') }}js/core-plugins.js"></script>
+	<script type="text/javascript" src="{{ asset('/') }}js/customs.js"></script>
+>>>>>>> fach bdlt navbar o zedt l carousel
 
-<!-- Only in Home Page -->
-<script type="text/javascript" src="{{ asset('/') }}js/jquery.flexdatalist.js"></script>
+	<!-- Only in Home Page -->
+	<script type="text/javascript" src="{{ asset('/') }}js/jquery.flexdatalist.js"></script>
 
+<<<<<<< HEAD
 @yield('script-details-car')
 
+=======
+	<!-- Detail Page JS -->
+	<script type="text/javascript" src="{{ asset('/') }}js/jquery.stickit.js"></script>
+	<script type="text/javascript" src="{{ asset('/') }}js/bootstrap-tokenfield.js"></script>
+	<script type="text/javascript" src="{{ asset('/') }}js/typeahead.bundle.min.js"></script>
+	<script type="text/javascript" src="{{ asset('/') }}js/jquery.sumogallery.js"></script>
+	<script type="text/javascript" src="{{ asset('/') }}js/images-grid.js"></script>
+	<script type="text/javascript" src="{{ asset('/') }}js/jquery.bootstrap-touchspin.js"></script>
+	<script type="text/javascript" src="{{ asset('/') }}js/customs-detail.js"></script>
+>>>>>>> fach bdlt navbar o zedt l carousel
 
 
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,107 +1,216 @@
 @extends('layouts.app')
-@section('content')
+ 
+ 
+ @section('content')
+ <style> 
+ /* .carousel { z-index: -99; } keeps this behind all content */
+ 
+.carousel .one {
+    background: url(/images/bg/bg8.jpg);
+	background-size: cover;
+	background-position: center;
+	background-repeat: no-repeat;
+    -moz-background-size: cover;
+}
+.carousel .two {
+    background: url(/images/bg/bg4.jpg);
+	background-size: cover;
+	background-repeat: no-repeat;
+
+	background-position: center;
+	
+    -moz-background-size: cover;
+}
+.carousel .three {
+    background: url(/images/bg/bg3.jpg);
+	background-position: center ;
+	background-repeat: no-repeat;
+	
+	background-size: cover;
+    -moz-background-size: cover;
+}
+.carousel .four {
+    background: url(/images/bg/bg7.jpg);
+	background-position: center ;
+	background-repeat: no-repeat;
+	
+	background-size: cover;
+    -moz-background-size: cover;
+}
+.carousel .active.left {
+    left:0;
+    opacity:0;
+    z-index:2;
+}</style>
+ 
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('.carousel').carousel({interval: 2000});
+  });
+</script>
+     <!-- Carousel
+                ================================================== -->
+				<div id="myCarousel" class="carousel slide hero" data-ride="carousel"
+		>
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+				
+                </ol>
+                <div class="carousel-inner" role="listbox">
+				<div class="item one active d-block w-100"  >
+					<!-- <img class="first-slide 
+					" src="{{ asset('/') }}images/bg/bg1.jpg"  alt="First slide">
+  -->
+                    <div class="carousel-caption">
+                        <h1>WELCOME</h1>
+						<p>BEST PLACE FOR YOU</p>
+						
+					</div>
+				 
+                </div>
+                <div class="item two d-block w-100" >
+                    <!-- <img class="second-slide" src="/images/bg/bg2.jpg" style="height:inherit"> -->
+
+                    <div class="carousel-caption">
+                        <h1>WELCOME 2</h1>
+                        <p>BEST CAR FOR YOU</p>
+                    </div>
+                </div>
+                <div class="item three d-block w-100">
+                    <!-- <img class="third-slide" src="{{ asset('/') }}images/hero-header/04.jpg" alt="Third slide"> -->
+
+                    <div class="carousel-caption">
+                        <h1>WELCOME 3</h1>
+                        <p>BEST HOUSE FOR YOU</p>
+                    </div>
+				</div>
+				<div class="item four d-block w-100">
+                    <!-- <img class="third-slide" src="{{ asset('/') }}images/hero-header/04.jpg" alt="Third slide"> -->
+
+                    <div class="carousel-caption">
+                        <h1>WELCOME 3</h1>
+                        <p>BEST HOUSE FOR YOU</p>
+                    </div>
+                </div>
+                </div>
+                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+                </a>
+            </div>
+            <!-- /.carousel -->
+               
 <div class="container">
 
-    <div class="post-hero clearfix">
+<div class="post-hero clearfix">
 
-        <div class="container">
+	<div class="container">
 
-            <div class="row">
+		<div class="row">
 
-                <div class="col-xs-12 col-sm-4 mb-20-xs">
-                    <div class="horizontal-featured-icon-sm clearfix">
-                        <div class="icon"><i class="ri ri-location"></i></div>
-                        <div class="content">
-                            <h6>Looking for a tour program?</h6>
-                            <span>Inhabiting discretion the her dispatched decisively boisterous joy.</span>
-                        </div>
-                    </div>
-                </div>
+			<div class="col-xs-12 col-sm-4 mb-20-xs">
+				<div class="horizontal-featured-icon-sm clearfix">
+					<div class="icon"><i class="ri ri-location"></i></div>
+					<div class="content">
+						<h6>Looking for a tour program?</h6>
+						<span>Inhabiting discretion the her dispatched decisively boisterous joy.</span>
+					</div>
+				</div>
+			</div>
 
-                <div class="col-xs-12 col-sm-4 mb-20-xs">
-                    <div class="horizontal-featured-icon-sm clearfix">
-                        <div class="icon"> <i class="ri ri-user"></i></div>
-                        <div class="content">
-                            <h6>Need someone to guide tour?</h6>
-                            <span>Great asked oh under together prospect kindness securing six.</span>
-                        </div>
-                    </div>
-                </div>
+			<div class="col-xs-12 col-sm-4 mb-20-xs">
+				<div class="horizontal-featured-icon-sm clearfix">
+					<div class="icon"> <i class="ri ri-user"></i></div>
+					<div class="content">
+						<h6>Need someone to guide tour?</h6>
+						<span>Great asked oh under together prospect kindness securing six.</span>
+					</div>
+				</div>
+			</div>
 
-                <div class="col-xs-12 col-sm-4 mb-20-xs">
-                    <div class="horizontal-featured-icon-sm clearfix">
-                        <div class="icon"> <i class="ri ri-equal-circle"></i></div>
-                        <div class="content">
-                            <h6>Want to earn money as guide?</h6>
-                            <span>Sometimes studied evident. Conduct replied removal her cordially. </span>
-                        </div>
-                    </div>
-                </div>
+			<div class="col-xs-12 col-sm-4 mb-20-xs">
+				<div class="horizontal-featured-icon-sm clearfix">
+					<div class="icon"> <i class="ri ri-equal-circle"></i></div>
+					<div class="content">
+						<h6>Want to earn money as guide?</h6>
+						<span>Sometimes studied evident. Conduct replied removal her cordially. </span>
+					</div>
+				</div>
+			</div>
 
-            </div>
+		</div>
 
-        </div>
-
-    </div>
-
-    <!---->
-
-    <div class="breadcrumb-image-bg  mx-bg-v2" style="background-image: url('/storage/uploads-mx/bg-car.jpg')">
-
-        <div class="container mx-container-3">
-
-            <div class="page-title">
-
-                <div class="mx-row row">
-
-                    <div class="col-sm-8  col-md-6 ">
-                        <div class="caption-mx-1" data-aos="zoom-out-left">
-                        <h2 class="txt1">VOITURES DE LUXE ET TRANSPORT TOURISTIQUE</h2>
-                        <p class="txt2">Nous vous proposons un large parc de voitures de luxe avec ou sans chauffeur
-                            Avec des services de transport touristique,
-                            Nous sommes certains que la qualité de nos services sera à la hauteur de vos attentes.</p>
-
-                        </div>
-
-                    <a  href="#">
-                        DECOUVRIR MAINTENANT
-                    </a>
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>
-
-    <div class="breadcrumb-image-bg  mx-bg-v2" style="background-image: url('/storage/uploads-mx/bg-home.jpg')">
-
-        <div class="container mx-container-3">
-
-            <div class="page-title">
-
-                <div class="mx-row row">
-
-                    <div class="col-sm-8  col-md-6 ">
-                        <div class="caption-mx-1" data-aos="zoom-out-left">
-                        <h2 class="txt1">IMMOBILIERS DE LUXE ET DE QUALITE</h2>
-                        <p class="txt2">Nous vous proposons des immobiliers de luxe à vendre ou à louer
-                            Nous sommes certains que la qualité de nos services sera à la hauteur de vos attentes.</p>
-
-                        </div>
-
-                    <a  href="#">
-                        DECOUVRIR MAINTENANT
-                    </a>
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>
+	</div>
 
 </div>
-@endsection
+
+<!---->
+
+<div class="breadcrumb-image-bg  mx-bg-v2" style="background-image: url('/storage/uploads-mx/bg-car.jpg')">
+
+	<div class="container mx-container-3">
+
+		<div class="page-title">
+
+			<div class="mx-row row">
+
+				<div class="col-sm-8  col-md-6 ">
+					<div class="caption-mx-1" data-aos="zoom-out-left">
+					<h2 class="txt1">VOITURES DE LUXE ET TRANSPORT TOURISTIQUE</h2>
+					<p class="txt2">Nous vous proposons un large parc de voitures de luxe avec ou sans chauffeur
+						Avec des services de transport touristique,
+						Nous sommes certains que la qualité de nos services sera à la hauteur de vos attentes.</p>
+
+					</div>
+
+				<a  href="#">
+					DECOUVRIR MAINTENANT
+				</a>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+<div class="breadcrumb-image-bg  mx-bg-v2" style="background-image: url('/storage/uploads-mx/bg-home.jpg')">
+
+	<div class="container mx-container-3">
+
+		<div class="page-title">
+
+			<div class="mx-row row">
+
+				<div class="col-sm-8  col-md-6 ">
+					<div class="caption-mx-1" data-aos="zoom-out-left">
+					<h2 class="txt1">IMMOBILIERS DE LUXE ET DE QUALITE</h2>
+					<p class="txt2">Nous vous proposons des immobiliers de luxe à vendre ou à louer
+						Nous sommes certains que la qualité de nos services sera à la hauteur de vos attentes.</p>
+
+					</div>
+
+				<a  href="#">
+					DECOUVRIR MAINTENANT
+				</a>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+</div>
+ 
+ @endsection
